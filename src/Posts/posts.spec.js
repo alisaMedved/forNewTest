e1bc849efc3f4bf6a5dfa82a97fd8e9126fbb0e5
Alice <adelaide3alice@gmail.com>
2020-11-11 20:05:23 +0500

    тесты запросов, mock ответа с сервера, mockReturnValue

--- src/Posts/posts.spec.js
+++ src/Posts/posts.spec.js
@@ -1,5 +1,20 @@
 import React from "react";
 import Posts from "./posts";
+import {
+  NEWS,
+  BASE_PATH,
+  SEARCH_PATH,
+  SEARCH_PARAM,
+  PAGE_HITS,
+  PAGE_PARAM,
+} from "./constants";
+import {jest} from "@jest/globals";
+
+// mock запроса
+// вот { hits: NEWS, page: 1, nbPages: 10 } - это то что прислал нам сервер - ну мы его замокали
+const mockJsonPromise = Promise.resolve({ hits: NEWS, page: 1, nbPages: 10 });
+const mockFetchPromise = Promise.resolve({ json: () => mockJsonPromise });
+global.fetch = jest.fn().mockImplementation(() => mockFetchPromise);
 
 // а в каком методе мы тестируем хендлеры и изменения стейта - достаточно shallow
 const setUp = () => shallow(<Posts />);
@@ -20,7 +35,62 @@ describe("Posts component", () => {
   expect(component).toMatchSnapshot();
   });
 
-  describe("Post handlers", () => {
+  it("should call fetch in componentDidMount", () => {
+    expect(global.fetch).toHaveBeenCalledWith(`${BASE_PATH}${SEARCH_PATH}?${SEARCH_PARAM}${""}&${PAGE_HITS}${20}&${PAGE_PARAM}${0}`)
+  });
+
+  describe("updatePage method", () => {
+
+    // чтобы страница в state менялась при изменении в Pagination
+    it("should update state 'page' value during call updatePage", () => {
+      instance.updatePage(DEFAULT_PAGE);
+      expect(component.state().page).toBe(DEFAULT_PAGE);
+    });
+
+it("should call fetch during call updatePage", () => {
+  instance.updatePage(DEFAULT_PAGE);
+  expect(global.fetch).toHaveBeenCalledWith(
+    `${BASE_PATH}${SEARCH_PATH}?${SEARCH_PARAM}${""}&${PAGE_HITS}${20}&${PAGE_PARAM}${DEFAULT_PAGE}`)
+});
+  });
+
+  describe("handlePageChange method", () => {
+
+    describe("should call updatePage with given argument during call handlePageChange", () => {
+
+      it("should call updatePage with given argument", () => {
+        // мокаем функцию updatePage
+        instance.updatePage = jest.fn();
+        // устанавливаем в state дефолтное значение
+        instance.setState({page: DEFAULT_PAGE});
+        // вот этой запутанной строкой мы добиваемся того чтобы вызвался handlePageChange
+        // с таким аргументом при котором btnType := 1
+        // особое внимание mockReturnValue
+        instance.handlePageChange({target: {getAttribute: jest.fn().mockReturnValue(1)}});
+        // правильно - должен сработать первый if и будет вызов
+        //       this.updatePage(+btnType);
+        expect(instance.updatePage).toHaveBeenCalledWith(1);
+      });
+
+      it("should call updatePage with increase page", () => {
+        instance.updatePage = jest.fn();
+        instance.setState({page: DEFAULT_PAGE});
+        instance.handlePageChange({target: {getAttribute: jest.fn().mockReturnValue("next")}});
+        expect(instance.updatePage).toHaveBeenCalledWith(DEFAULT_PAGE + 1);
+      });
+
+      it("should call updatePage with decrease page", () => {
+        instance.updatePage = jest.fn();
+        instance.setState({page: DEFAULT_PAGE});
+        instance.handlePageChange({target: {getAttribute: jest.fn().mockReturnValue("prev")}});
+        expect(instance.updatePage).toHaveBeenCalledWith(DEFAULT_PAGE - 1);
+      });
+    });
+
+  });
+
+
+    describe("Post handlers", () => {
     it("should handle search input value", () => {
       // проверяем что перед запуском хендлера значение реально пустое в стейте
       expect(component.state().searchQuery).toBe("");
